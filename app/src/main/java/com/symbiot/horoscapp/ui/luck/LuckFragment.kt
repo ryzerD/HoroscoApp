9bd85b974e8fb81ac9a1fcc85e8764e95da67013
davidfguer <dfigueroa@tiresia.co>
2023-11-29 09:30:13 -0500

    Animation Completed

--- app/src/main/java/com/symbiot/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/symbiot/horoscapp/ui/luck/LuckFragment.kt
@@ -1,13 +1,27 @@
 package com.symbiot.horoscapp.ui.luck
 
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.snackbar.Snackbar
+import com.symbiot.horoscapp.R
 import com.symbiot.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
@@ -23,6 +37,113 @@ class LuckFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentLuckBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+        val network: Boolean = isNetworkAvailable(requireContext())
+        if (!network) {
+            showNoInternetSnackbar()
+        }
+    }
+
+    private fun initListeners() {
+        binding.IvRoulette.setOnClickListener {
+            spinRoulette()
+        }
+    }
+
+    private fun spinRoulette() {
+        val random = Random()
+        val degress = random.nextInt(1440) + 360
+        val animator =
+            ObjectAnimator.ofFloat(binding.IvRoulette, View.ROTATION, 0f, degress.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
+        animator.start()
+    }
+
+    private fun slideCard() {
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+        slideUpAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                binding.reverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                growCard()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
+        binding.reverse.startAnimation(slideUpAnimation)
+    }
+
+    private fun growCard() {
+        val growAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.grow_up)
+        growAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.reverse.isVisible = false
+                showPredictionView()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+        })
+        binding.reverse.startAnimation(growAnimation)
+    }
+
+    private fun showPredictionView() {
+        val disappearAnimation = AlphaAnimation(1.0f, 0.0f)
+        val apearAnimation = AlphaAnimation(0.0f,1.0f)
+        apearAnimation.duration = 1000
+        disappearAnimation.duration = 200
+        disappearAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.preview.isVisible = false
+                binding.predict.isVisible = true
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        binding.preview.startAnimation(disappearAnimation)
+        binding.predict.startAnimation(apearAnimation)
+    }
+
+
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return activeNetwork.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
+    private fun showNoInternetSnackbar() {
+        val snackbar = Snackbar.make(binding.root, "No hay conexión", Snackbar.LENGTH_LONG)
+        snackbar.view.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(),
+                android.R.color.holo_red_dark
+            )
+        )
+        snackbar.show()
     }
 
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.2.0-rc03" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id ("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0" apply false
